737e6d3d4d4fe76715a677f0d15ec882279e0471
Alexander <liujalexander84@gmail.com>
2020-07-05 14:02:29 -0700

    set bot status
    can be configged (later) for random status

--- index.js
+++ index.js
@@ -80,6 +80,12 @@ autumnblaze.connect = () => {
          console.warn("if this pops up then something is def wrong");
       }
       console.log("connection success!!");
+      autumnblaze.bot.user.setPresence({
+         activity: {
+            name: "\"" + autumnblaze.opts.prefix + "help\"",
+            type: 0
+         }
+      });
    }).catch(err => {
       console.log("connection failed lol");
       console.log(err);
