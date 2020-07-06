0c62957562a9b88539eaa33df68225b551af5176
Alexander <liujalexander84@gmail.com>
2020-07-06 14:23:15 -0700

    fixed noarg commands

--- text/commands/_process.js
+++ text/commands/_process.js
@@ -1,13 +1,28 @@
 module.exports = (message, autumnblaze) => {
-   // loggie
-   console.log(autumnblaze.randutils.logcmdmsg(message));
    // strip out the prefix, and process it
    const sentcmd = message.content.substring(autumnblaze.opts.prefix.length);
 
-   for (const cmd in autumnblaze.commands) if (sentcmd.substring(0, cmd.length + 1) === cmd + " ") {
-      let response = autumnblaze.commands[cmd](sentcmd.substring(cmd.length + 1));
-      // need to check response for nullness and undefinedness lol
-      message.channel.send(response);
-      return;
+   for (const cmd in autumnblaze.commands) {
+      if (sentcmd === cmd) {
+         // loggie
+         console.log(autumnblaze.randutils.logcmdmsg(message));
+
+         // exactly the command, no args
+         const response = autumnblaze.commands[cmd]("");
+         if (response) message.channel.send(response);
+         return;
+      }
+      if (sentcmd.substring(0, cmd.length + 1) === (cmd + " ")) {
+         // loggie
+         console.log(autumnblaze.randutils.logcmdmsg(message));
+
+         // has space between cmd and args lol
+         // this caused me so much issues lol
+         const response = autumnblaze.commands[cmd](sentcmd.substring(cmd.length + 1));
+         if (response) message.channel.send(response);
+         return;
+      }
    }
+   // if here its not a command
+   console.log(autumnblaze.randutils.logmsg(message));
 };
