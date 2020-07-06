35bdf89bcafcde74b86647bc60aa61e2c0c0770f
Alexander <liujalexander84@gmail.com>
2020-07-06 14:47:37 -0700

    random array randutil thingie

--- randutils/index.js
+++ randutils/index.js
@@ -3,5 +3,6 @@ var randutils = {};
 randutils.copyobj = require("./copyobj");
 randutils.logmsg = require("./logmsg");
 randutils.logcmdmsg = require("./logcmdmsg");
+randutils.randfromarray = require("./randfromarray");
 
 module.exports = randutils;
---
+++ randutils/randfromarray.js
@@ -0,0 +1,8 @@
+module.exports = (ar, ar2 = null, ar3 = null, ar4 = null) => {
+   // assuming all arrays are the same length
+   const i = Math.floor(Math.random() * ar.length);
+   if (ar4 != null) return [ar[i], ar2[i], ar3[i], ar4[i]];
+   if (ar3 != null) return [ar[i], ar2[i], ar3[i]];
+   if (ar2 != null) return [ar[i], ar2[i]];
+   return ar[i];
+};
