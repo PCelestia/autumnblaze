b404ad8563581383946360d9a4ec3f5e5d822caa
Alexander <liujalexander84@gmail.com>
2020-07-05 17:16:31 -0700

    dont use brackets to surround sent message, and reduce duplicate

--- randutils/logmsg.js
+++ randutils/logmsg.js
@@ -1,3 +1,3 @@
 module.exports = msg => {
-   return "[" + msg.content + "]" + " author[" + msg.author.username + "#" + msg.author.discriminator + "][" + msg.author.id + "] guild[" + msg.guild + "] channel[" + msg.channel + "]";
+   return "\"" + msg.content + "\"" + " author[" + msg.author.username + "#" + msg.author.discriminator + "][" + msg.author.id + "] guild[" + msg.guild + "] channel[" + msg.channel + "]";
 };
