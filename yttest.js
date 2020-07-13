417773ca9682b57fc4d64689691ff6ebfbe2592c
Alexander Liu <liujalexander84@gmail.com>
2020-07-12 21:08:07 -0700

    rm yttest.js, invalidated api key as well

--- yttest.js
+++
@@ -1,4 +0,0 @@
-const yas = require("youtube-audio-server");
-yas.setKey("AIzaSyDiT8b8tmIOPir-g5XfzEliLqEUcVV6f3Y");
-const port = 8005;
-yas.listen(port, () => console.log("listenin' port " + port));
