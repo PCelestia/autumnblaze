ea7d6dc513437e89af173c1b9795e4cb401f10a1
Alexander Liu <liujalexander84@gmail.com>
2020-07-14 18:10:40 -0700

    include github repo link in embed

--- text/commands/about.js
+++ text/commands/about.js
@@ -20,6 +20,7 @@ const thecmd = async cmd => {
 
    desc = desc + " as " + autumnblaze.bot.user.username + "#" + autumnblaze.bot.user.discriminator;
    if (autumnblaze.opts.location) desc = desc + "\nhosted in " + autumnblaze.opts.location;
+   desc = desc + "\nGitHub repository: [pcelestia/autumnblaze](https://github.com/pcelestia/autumnblaze/ \"GitHub repository\")";
    if (cmd !== "") desc = desc + "\nalso i got the rest, it just doesnt do anything ~~yet~~";
    embed.setDescription(desc);
 
