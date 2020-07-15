106cb13cd317be971a56c8954eb5eaa465790fee
Alexander Liu <liujalexander84@gmail.com>
2020-07-14 20:19:20 -0700

    use prefix elsewhere, but do not use prefix when dming

--- text/commands/_process.js
+++ text/commands/_process.js
@@ -1,12 +1,18 @@
 "use strict";
 
 module.exports = (message, autumnblaze) => {
-   // strip out the prefix, and process it
-   const sentcmd = message.content.substring(autumnblaze.opts.prefix.length);
+   // strip out the prefix if not dm, dms cannot use prefix
+   let sentcmd = message.content;
+   let dm = false;
+
+   // ignore thyself
+   if (message.author === autumnblaze.bot.user) return;
+   if (message.channel.type === "dm") {
+      dm = true;
+   } else sentcmd = sentcmd.substring(autumnblaze.opts.prefix.length);
 
    for (const cmd in autumnblaze.commands) {
       if (sentcmd === cmd) {
-
          // exactly the command, no args
          const response = autumnblaze.commands[cmd]("");
          Promise.resolve(response).then(val => {
@@ -18,7 +24,6 @@ module.exports = (message, autumnblaze) => {
          return;
       }
       if (sentcmd.substring(0, cmd.length + 1) === (cmd + " ")) {
-
          // has space between cmd and args lol
          // this caused me so much issues lol
          const response = autumnblaze.commands[cmd](sentcmd.substring(cmd.length + 1));
@@ -30,6 +35,7 @@ module.exports = (message, autumnblaze) => {
          });
          return;
       }
-
    }
+   // no command found, if reg channel is ok, if dm then send error
+   if (dm) message.channel.send("command not found");
 };
