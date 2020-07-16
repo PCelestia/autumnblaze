49be7789b20315eeeb3174ce6d41f98000190127
Alexander Liu <liujalexander84@gmail.com>
2020-07-15 23:09:13 -0700

    respond to pings, reduce code duping, typing indicator

--- text/commands/_process.js
+++ text/commands/_process.js
@@ -1,9 +1,14 @@
 "use strict";
 
 module.exports = (message, autumnblaze) => {
+   // react to ping lol
    const leet = autumnblaze.randutils.botpinged(message);
-   if (leet[0]) if (leet[1]) {
-      message.channel.send("you called?\nmy prefix is `autumn `, for example `autumn help`");
+   if (leet[0]) {
+      if (leet[1]) {
+         message.channel.send("you called?\nmy prefix here is `autumn `, for example `autumn help`");
+         return;
+      }
+      message.channel.send("i see i've been pinged");
       return;
    }
    // strip out the prefix if not dm, dms cannot use prefix
@@ -20,27 +25,29 @@ module.exports = (message, autumnblaze) => {
       if (sentcmd === cmd) {
          // exactly the command, no args
          const response = autumnblaze.commands[cmd]("", message);
-         Promise.resolve(response).then(val => {
-            if ((val !== undefined) && (val !== "")) message.channel.send(val).catch(console.warn);
-         }).catch(err => {
-            console.log("err processing cmd " + cmd);
-            console.log(err);
-         });
+         goham(message, response, cmd);
          return;
       }
       if (sentcmd.substring(0, cmd.length + 1) === (cmd + " ")) {
          // has space between cmd and args lol
          // this caused me so much issues lol
          const response = autumnblaze.commands[cmd](sentcmd.substring(cmd.length + 1), message);
-         Promise.resolve(response).then(val => {
-            if ((val !== undefined) && (val !== "")) message.channel.send(val).catch(console.warn);
-         }).catch(err => {
-            console.log("err processing cmd " + cmd);
-            console.log(err);
-         });
+         goham(message, response, cmd);
          return;
       }
    }
    // no command found, if reg channel ignore, if dm then send error
    if (dm) message.channel.send("command not found").catch(console.log);
+};
+
+const goham = (message, response, cmd) => {
+   message.channel.startTyping();
+   Promise.resolve(response).then(val => {
+      if ((val !== undefined) && (val !== "")) message.channel.send(val).catch(console.warn);
+   }).catch(err => {
+      console.log("err processing cmd " + cmd);
+      console.log(err);
+   }).finally(() => {
+      message.channel.stopTyping();
+   });
 };
