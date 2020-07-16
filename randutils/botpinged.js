5795a51ef96d3cc1c251a90bcd17a585e78c7749
Alexander Liu <liujalexander84@gmail.com>
2020-07-16 14:02:19 -0700

    add whether or not message starts with bot ping to randutils/botpinged

--- randutils/botpinged.js
+++ randutils/botpinged.js
@@ -3,8 +3,8 @@
 module.exports = msg => {
    // msg is discordjs msg object
    // rv[0] = if pinged
-   // if true, then rv[1] = whether or not its only the pingstring
+   // if true, then rv[1] = whether or not its only the pingstring and rv[2] = whether or not it starts with pingstring
    const pingstring = "<@!" + msg.client.user.id + ">";
-   if (msg.content.includes(pingstring)) return [true, msg.content === pingstring];
+   if (msg.content.includes(pingstring)) return [true, msg.content === pingstring, msg.content.startsWith(pingstring)];
    return [false];
 };
