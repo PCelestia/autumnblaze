2c0d0a6cb9be8e3995253e1060d73d022a2a7be7
Alexander Liu <liujalexander84@gmail.com>
2020-07-18 14:02:53 -0700

    checksubcmd commentz

--- randutils/checksubcmd.js
+++ randutils/checksubcmd.js
@@ -1,4 +1,6 @@
 "use strict";
+// rv[0] whether or not is a match
+// if match rv[1] is rest of args (excluding the space)
 
 module.exports = (str, subcmd) => {
    if (!str.startsWith(subcmd)) return [false];
