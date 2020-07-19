1217d754e5b27e635e74ee6904d1b8674769ff24
Alexander Liu <liujalexander84@gmail.com>
2020-07-18 22:41:11 -0700

    lol unnecessary file

--- randutils/checkperms.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = (msg) => {
-   const allperms = require("discord.js").Permissions.FLAGS;
-   const hasperms = [];
-
-   for (const perm in allperms) if (msg.member.hasPermission(perm)) hasperms.push(perm);
-   return hasperms;
-};
