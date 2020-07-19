e4be54e9aaceb25cf5395dce6b2a899cb1407cff
Alexander Liu <liujalexander84@gmail.com>
2020-07-18 22:42:05 -0700

    give randutils the fs treatment

--- randutils/index.js
+++ randutils/index.js
@@ -2,12 +2,14 @@
 // i know i could use the same method to autofill everything as i did with commands,
 // but these aren't gonna grow so fast and its easier to just do it this way
 // when it gets big enough i will
+const fs = require("fs");
+const path = require("path");
 var randutils = {};
 
-randutils.copyobj = require("./copyobj");
-randutils.randfromarray = require("./randfromarray");
-randutils.botpinged = require("./botpinged");
-randutils.checkperms = require("./checkperms");
-randutils.checksubcmd = require("./checksubcmd");
+const randutilfiles = fs.readdirSync(path.resolve(__dirname, ".")).filter(file => file.endsWith(".js") && file !== "index.js");
+randutilfiles.forEach(randutil => {
+   const randutilnoext = randutil.slice(0, randutil.length - 3);
+   randutils[randutilnoext] = require("./" + randutil);
+});
 
 module.exports = randutils;
