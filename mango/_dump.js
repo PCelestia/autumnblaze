47996f2e47bb7df7a552ce0964a9d8c8e50dbae5
Alexander Liu <liujalexander84@gmail.com>
2020-07-25 22:11:37 -0700

    more detailed dump lol

--- mango/_dump.js
+++ mango/_dump.js
@@ -1,6 +1,9 @@
 module.exports = async autumnblaze => {
-   if (autumnblaze.opts.allowcache) {
-      console.log("dumpppppppp");
-      console.log(require("./_cache"));
+   console.log("dumpppppppp");
+   // temp dump everything to console
+   const cache = require("./_cache");
+   for (const key1 in cache) for (const key2 in cache[key1]) for (const key3 in cache[key1][key2]) {
+      console.log(key1 + "." + key2 + "." + key3 + ": " + cache[key1][key2][key3]);
+      if (key3 === "the") for (const key4lol in cache[key1][key2][key3]) console.log("key4444." + key4lol + "." + cache[key1][key2][key3][key4lol]);
    }
 };
