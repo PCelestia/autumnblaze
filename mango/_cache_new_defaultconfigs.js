9f8abc051ab0fafe28ec5ded5f0a815378da4955
Alexander Liu <liujalexander84@gmail.com>
2020-07-27 14:06:17 -0700

    code deduplicationn

--- mango/_cache_new_defaultconfigs.js
+++ mango/_cache_new_defaultconfigs.js
@@ -1,14 +1,3 @@
 "use strict";
-// lol i guess if a field is undefined it uses the default
 
-const defaultguildsettings = {
-   name: "guildsettings"
-};
-const defaultusersettings = {
-   name: "usersettings"
-};
-
-module.exports = {
-   defaultguildsettings: defaultguildsettings,
-   defaultusersettings: defaultusersettings
-};
+module.exports = require("./defaultconfigs");
