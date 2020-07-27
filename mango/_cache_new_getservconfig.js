fe563143883205a1720e9d9a72aa6494780bafb7
Alexander Liu <liujalexander84@gmail.com>
2020-07-26 23:20:30 -0700

    DO NOT USE YET getservconfig from cache, query db if not available

--- mango/_cache_new_getservconfig.js
+++ mango/_cache_new_getservconfig.js
@@ -1,22 +1,16 @@
 "use strict";
 
 module.exports = (mongodatabase, guild, callback) => {
+   // check the cache
+   const cache = require("./_cache");
+   const cacheconfig = cache.get(guild.id, "guildsettings");
+   if (cacheconfig) return cacheconfig;
+
+   // noep
    console.log("a query has baen made");
-   mongodatabase.collection(guild.id).findOne({ name: "guildsettings" }, (err, res) => {
-      if (err) {
-         // err
-         console.warn("mongoerror in getting server config");
-         console.warn(err);
-         callback(undefined);
-         return;
-      }
-      if (res) {
-         // gottem
-         callback(res);
-         return;
-      } else {
-         // not gottem
-         require("./createdefaultservconfig")(mongodatabase, guild, callback);
-      }
+   require("./getservconfig")(mongodatabase, guild, val => {
+      if (!val) return callback(undefined);
+      cache.set(guild.id, "guildsettings", val);
+      callback(require("../randutils").copyobj(val));
    });
 };
