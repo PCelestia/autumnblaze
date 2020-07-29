6ab6809f390793e83dcb4fd2c4326ee11a29d6ee
Alexander Liu <liujalexander84@gmail.com>
2020-07-28 19:59:46 -0700

    get all configs, make isdm, config available to command handlers

--- text/commands/_process.js
+++ text/commands/_process.js
@@ -6,22 +6,30 @@ module.exports = async (message, autumnblaze) => {
 
    // see if dm
    const dm = message.channel.type === "dm";
+   let config;
    if (!dm) {
       // (try to) remove prefix if guild
-      const prefix = await new Promise((resolve, reject) => {
-         autumnblaze.mango.getservconfig(autumnblaze.db, message.guild, servconfig => {
-            if (servconfig === undefined) return reject("failed to create/get server config");
-            if (servconfig.prefix === undefined) return resolve(autumnblaze.opts.prefix);
-            resolve(servconfig.prefix);
+      config = await new Promise((resolve, reject) => {
+         autumnblaze.mango.getservconfig(autumnblaze.db, message.guild, config => {
+            if (config === undefined) return reject("failed to create/get server config");
+            if (config.prefix === undefined) config.prefix = autumnblaze.opts.prefix;
+            resolve(config);
          });
       });
       if (autumnblaze.randutils.botpinged(message)[0]) {
          let pingstring = "i see i've been pinged\n";
-         pingstring = pingstring + "my prefix here is `" + prefix + "`, for example `" + prefix + "help`";
+         pingstring = pingstring + "my prefix here is `" + config.prefix + "`, for example `" + config.prefix + "help`";
          message.channel.send(pingstring);
       }
-      if (sentcmd.startsWith(prefix)) sentcmd = sentcmd.substring(prefix.length);
+      if (sentcmd.startsWith(config.prefix)) sentcmd = sentcmd.substring(config.prefix.length);
       else return;
+   } else {
+      config = await new Promise((resolve, reject) => {
+         autumnblaze.mango.getuserconfig(autumnblaze.db, message.author, config => {
+            if (config === undefined) return reject("failed to create/get user config");
+            resolve(config);
+         });
+      });
    }
    for (const cmd in autumnblaze.commands) {
       // process sentcmd, if it ain't then continue
@@ -32,12 +40,12 @@ module.exports = async (message, autumnblaze) => {
       if (dm) {
          if (autumnblaze.commands[cmd].allowdm !== true) continue;
          // is dm, allowed to run in dm
-         respond(cmd, sentcmd, message, autumnblaze);
+         respond(cmd, sentcmd, message, autumnblaze, dm, config);
          return;
       } else {
          if (autumnblaze.commands[cmd].allowguild !== true) continue;
          // is guild, allowed to run in guild
-         respond(cmd, sentcmd, message, autumnblaze);
+         respond(cmd, sentcmd, message, autumnblaze, dm, config);
          return;
       }
    }
@@ -52,7 +60,7 @@ const cmdnotfound = async (msg) => {
    msg.channel.stopTyping();
 };
 
-const respond = async (cmd, arg, msg, autumnblaze) => {
+const respond = async (cmd, arg, msg, autumnblaze, dm, config) => {
    // if perms prop, check it, if none, assume everyone allowed to use it
 
    msg.channel.startTyping();
@@ -63,7 +71,7 @@ const respond = async (cmd, arg, msg, autumnblaze) => {
          // perms = await autumnblaze.randutils.hasperms(msg, ...autumnblaze.commands[cmd].perms);
          await autumnblaze.randutils.hasperms(msg, ...autumnblaze.commands[cmd].perms);
       }
-      return autumnblaze.commands[cmd](arg, msg, autumnblaze);
+      return autumnblaze.commands[cmd](arg, msg, autumnblaze, dm, config);
    })()).then(val => {
       if ((val !== undefined) && (val !== "")) {
          if (Array.isArray(val)) {
