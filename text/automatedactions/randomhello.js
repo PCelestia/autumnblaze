2e178fd30e3adba7eb69c7089f5f524548982071
Alexander Liu <liujalexander84@gmail.com>
2020-08-04 20:29:18 -0700

    *slightly* varied hello response

--- text/automatedactions/randomhello.js
+++ text/automatedactions/randomhello.js
@@ -1,3 +1,3 @@
-module.exports = message => {
-   if (message.content.toLowerCase().split(/ +/).includes("hi")) message.channel.send("Hello!");
+module.exports = (message, _, autumnblaze) => {
+   if (message.content.toLowerCase().split(/ +/).includes("hi")) message.channel.send(autumnblaze.randutils.randfromarray(["Hello!", "Hi", "Welcome back", "Hey, how are you today?"]));
 };
