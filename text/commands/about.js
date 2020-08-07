a220b95bf679d1c74bed533e755229e01c48a86a
Alexander Liu <liujalexander84@gmail.com>
2020-08-06 23:22:01 -0700

    rm "in" from about

--- text/commands/about.js
+++ text/commands/about.js
@@ -17,7 +17,7 @@ const thecmd = async (cmd, _, autumnblaze) => {
    else desc = desc + ", running";
 
    desc = desc + " as " + autumnblaze.bot.user.username + "#" + autumnblaze.bot.user.discriminator;
-   if (autumnblaze.opts.location) desc = desc + "\nhosted in " + autumnblaze.opts.location;
+   if (autumnblaze.opts.location) desc = desc + "\nhosted " + autumnblaze.opts.location;
    // desc = desc + "\nGitHub repository: [pcelestia/autumnblaze](https://github.com/pcelestia/autumnblaze/ \"GitHub repository\")";
    if (cmd !== "") desc = desc + "\nalso i got the rest, it just doesnt do anything ~~yet~~";
    embed.setDescription(desc);
