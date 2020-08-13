4462145ef0f2ee70fe60489ef1afaee2a2ebfb64
Alexander Liu <liujalexander84@gmail.com>
2020-08-12 20:11:49 -0700

    me arr

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,7 @@
 "use strict";
 
-module.exports = autumnblaze => {
-   return member => {
-      autumnblaze.mango.promise.getservconfig(autumnblaze.db, member.guild).then(servconfig => {
-         if (servconfig.welcomechannel) autumnblaze.bot.channels.fetch(servconfig.welcomechannel).then(channel => channel.send("Welcome <@" + member.id + ">!"));
-      }).catch(console.warn);
-   };
+module.exports = autumnblaze => member => {
+   autumnblaze.mango.promise.getservconfig(autumnblaze.db, member.guild).then(servconfig => {
+      if (servconfig.welcomechannel) autumnblaze.bot.channels.fetch(servconfig.welcomechannel).then(channel => channel.send("Welcome <@" + member.id + ">!"));
+   }).catch(console.warn);
 };
