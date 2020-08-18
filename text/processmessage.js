2768f975ed67b2006c336e480eb559f8ca6955a3
Alexander Liu <liujalexander84@gmail.com>
2020-08-18 12:21:54 -0700

    rm typing for h

--- text/processmessage.js
+++ text/processmessage.js
@@ -10,10 +10,8 @@ module.exports = async (message, autumnblaze) => {
       // h (idea) by Thorinair on A State of Sugar Discord server
       // github username: thorinair
       // (temporary) sloppy reimplementation by me lol
-      message.channel.startTyping();
       autumnblaze.h = Date.now();
       message.channel.send("h");
-      message.channel.stopTyping();
       return;
    }
 
