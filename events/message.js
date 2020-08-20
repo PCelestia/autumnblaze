05d037acff7155e02a3e775e909bf7dbedfd2621
Alexander Liu <liujalexander84@gmail.com>
2020-08-19 22:34:44 -0700

    simplify a bit lol

--- events/message.js
+++ events/message.js
@@ -1,5 +1,3 @@
 "use strict";
 
-module.exports = autumnblaze => message => {
-   autumnblaze.text.processmessage(message, autumnblaze);
-};
+module.exports = autumnblaze => message => autumnblaze.text.processmessage(message, autumnblaze);
